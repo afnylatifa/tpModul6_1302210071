c99dd888fb3f5474978338b7632db727b42146b6
afny16 <afnylatifa@gmail.com>
2023-03-26 04:06:56 +0700

    menambahkan class SayaTubeVideo

--- tpModul6_1302210071/Program.cs
+++ tpModul6_1302210071/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random random = new Random();
+        this.title = title;
+        id = random.Next(10000, 99999);
+        playCount = 0;
+    }
+
+    public void IncreasePlayCount(int playCount)
+    {
+        this.playCount = playCount;
+    }
+
+    public void PrintVideoDetail()
+    {
+        Console.WriteLine("Detail video : ");
+        Console.WriteLine("ID : " + id);
+        Console.WriteLine("Title : " + title);
+        Console.WriteLine("Play count : " + playCount);
+    }
+}
+
+public class program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo sayaTubeVideo = new SayaTubeVideo("Tutorial Design By Contract - [Afny Latifa]");
+        sayaTubeVideo.PrintVideoDetail();
+    }
+}
